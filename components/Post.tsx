fbcaf818569d1858e2691fec02b54fbc1b5b8e2e
Vinay Negi <vinegi004@gmail.com>
2024-02-29 21:18:41 +0530

    Add post screen

--- components/Post.tsx
+++ components/Post.tsx
@@ -2,14 +2,27 @@ import { View, Text } from 'react-native'
 import { FontAwesome } from '@expo/vector-icons';
 import React from 'react'
 
-export default function Post() {
+export default function Post({item}:any){
+
+  if(item.id!=="aaaa")
+  {
   return (
-    <View className='bg-white mt-2 p-2' style={{height:"50%"}}>
-      <View className='bg-red-300 h-14 flex-row p-1'>
-        <View className=' w-14 bg-gray-300 justify-center items-center rounded-full'>
+    <View className='bg-white mt-2 p-2  h-44'>
+      <View className=' h-14 flex-row p-1 '>
+        <View className='w-12 bg-gray-300 justify-center items-center rounded-full'>
         <FontAwesome name="user" size={34} color="white" />
+        </View>
+        <View className='  ml-3'>
+        <Text className=' font-semibold text-lg '>{item.username}</Text>
+        <Text className='text-md'>{item.info}</Text>
+        </View>
+      </View>
+      <View className=' h-24'>
+        <View className=' p-1'>
+          <Text className=' text-lg'>{item.description}</Text>
         </View>
       </View>
     </View>
   )
+  }
 }
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,17 +1,51 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
 import Header from '../components/Header'
 import Navigation from '../components/Navigation'
 import Post from '../components/Post'
 
 const HomeScreen = () => {
+  type PostData={
+    username:string,
+    info:string,
+    description:string,
+    id:string
+  }
+
+  const dummyData:PostData[]=[{
+    username:"User 1",
+    info:"Mobile app developer",
+    description:"I have completed the work on my latest project",
+    id:"aaaa"
+  },
+  {
+    username:"User 2",
+    info:"Backend developer",
+    description:"Just use php bro",
+    id:"bbbb",
+  },
+  {
+    username:"User 3",
+    info:"Frontend developer",
+    description:"Ai will take over my job",
+    id:"cccc"
+  }
+  ]
+
+  const render=function(item:PostData)
+  {
+    return(
+      <Post item={item}/>
+    )
+  }
+
   return (
     <View className=' bg-gray-200 flex-1'>
       <View>
       <Header/>
       </View>  
       <View className='flex-1'>
-        <Post/>
+        <FlatList data={dummyData} renderItem={({item})=>render(item)}/>
       </View>
       <View className='h-14 bg-white'>
         <Navigation/>
